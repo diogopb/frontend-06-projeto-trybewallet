aaae4e2c2b0861fb612a5944be793456bd7a92b7
Diogo Pinto <dpintobarbosa@gmail.com>
2024-02-05 08:37:39 -0300

    Requisito 2

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,5 +1,22 @@
+import { useSelector } from 'react-redux';
+
+type GlobalState = {
+  user: {
+    email: '',
+  }
+
+};
+
 function Wallet() {
-  return <div>TrybeWallet</div>;
+  const rootState = useSelector((state: GlobalState) => state);
+
+  return (
+    <header data-testid="email-field">
+      <p data-testid="total-field">0</p>
+      <p data-testid="header-currency-field">BRL</p>
+      <p data-testid="email-field">{rootState.user.email}</p>
+    </header>
+  );
 }
 
 export default Wallet;
