d1cf992f6c014603bc8e99ac40b5103d00a829ae
Diogo Pinto <dpintobarbosa@gmail.com>
2024-02-07 11:56:50 -0300

    Requisito 5

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,46 @@
+import { useSelector } from 'react-redux';
+import { UserAndWalletType } from '../types';
+
 function Table() {
-  return <div>Table</div>;
+  const expenses = useSelector((state: UserAndWalletType) => state.wallet.expenses);
+
+  const bodyTable = expenses.map((expense: any) => {
+    const valueExp = Number(expense.exchangeRates[expense.currency].ask)
+      * Number(expense.value);
+    return (
+      <tr key={ expense.id }>
+        <td>{ expense.description }</td>
+        <td>{ expense.tag }</td>
+        <td>{ expense.method }</td>
+        <td>{ Number(expense.value).toFixed(2) }</td>
+        <td>{ expense.exchangeRates[expense.currency].name }</td>
+        <td>{ Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
+        <td>{ valueExp.toFixed(2) }</td>
+        <td>Real</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>{bodyTable}</tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Table;
